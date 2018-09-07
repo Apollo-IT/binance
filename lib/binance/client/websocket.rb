41e023c79eafee88b687081b68b4756e8ff00ec0
Apollo IT <apollo447722@outlook.com>
2018-09-07 22:10:32 +0800

    added auto-reconnection when close
    added auto-reconnection when close

--- lib/binance/client/websocket.rb
+++ lib/binance/client/websocket.rb
@@ -194,7 +194,7 @@ module Binance
       #   :close   - The Proc called when a stream is closed (optional)
       def create_stream(url, methods:)
         Faye::WebSocket::Client.new(url)
-                               .tap { |ws| attach_methods(ws, methods) }
+                               .tap { |ws| attach_methods(url, ws, methods) }
       end
 
       # Internal: Iterate through methods passed and add them to the WebSocket
@@ -207,9 +207,13 @@ module Binance
       #   :message - The Proc called when a stream receives a message
       #   :error   - The Proc called when a stream receives an error (optional)
       #   :close   - The Proc called when a stream is closed (optional)
-      def attach_methods(websocket, methods)
+      def attach_methods(url, websocket, methods)
         methods.each_pair do |key, method|
-          websocket.on(key) { |event| method.call(event) }
+          websocket.on(key) do |event|
+            method.call(event)
+            create_stream(url, methods: methods) if event == :close
+          end
+          # websocket.on(key) { |event| method.call(event) }
         end
       end
     end
